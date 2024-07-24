d24065aa3af95bf059b970877b2caef58c18b61d
ipatel830 <ishanpatel830@gmail.com>
2024-07-23 23:08:56 -0400

    First Push

---
+++ ish_Lab9.py
@@ -0,0 +1,15 @@
+def encode(password):
+    encoded = ''
+    for i in password:
+        i = int(i)
+        encoded += str(i+3)
+    
+    return encoded
+
+def decode(password):
+    decoded = ''
+    for i in password:
+        i = int(i)
+        decoded += str(i-3)
+    return decoded
+
